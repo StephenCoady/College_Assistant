737c0f6f9d87b59ee5ae2e8a71ea4f548b782d42
scoady2 <scoady2@gmail.com>
2015-11-05 21:57:13 +0000

    added assignment and module addition features. users can now view a list of all assignments. bug fixes.

--- myApp/app.js
+++ myApp/app.js
@@ -1,217 +1,328 @@
 var app = angular.module('collegeApp', ['ngRoute'])
 
 app.config(['$routeProvider',
-      function ($routeProvider, $rootScope) {
-        $routeProvider
-          .when('/', {
-            templateUrl: './partials/landing.html'
-          })
-          .when('/login', {
-            templateUrl: './partials/login.html',
-            controller: "loginCtrl"
-          })
-          .when('/modules/:moduleId', {
-            resolve: {
-              "check": function($location, $rootScope) {
-                if(!$rootScope.loggedIn){
-                  $location.path('/');
-                }
-              }
-            },
-            templateUrl: './partials/module-detail.html',
-            controller: "moduleCtrl"
-          })
-          .when('/modules/:moduleId/:assignId', {
-            resolve: {
-              "check": function($location, $rootScope) {
-                if(!$rootScope.loggedIn){
-                  $location.path('/');
-                }
-              }
-            },
-            templateUrl: './partials/assign-detail.html',
-            controller: "assignCtrl"
-          })
-          .when('/signup', {
-            templateUrl: './partials/signup.html',
-            controller: "NewUserController"
-          })
-          .when('/dashboard', {
-            resolve: {
-              "check": function($location, $rootScope) {
-                if(!$rootScope.loggedIn){
-                  $location.path('/');
-                }
-              }
-            },
-            templateUrl: './partials/dashboard.html',
-            controller: "dashboardCtrl"
-          })
-          .otherwise({
-            redirectTo: '/'
-          });
-      }]);
+  function ($routeProvider, $rootScope) {
+    $routeProvider
+    .when('/', {
+      templateUrl: './partials/landing.html'
+    })
+    .when('/login', {
+      templateUrl: './partials/login.html',
+      controller: "loginCtrl"
+    })
+    .when('/modules', {
+      resolve: {
+        "check": function($location, $rootScope) {
+          if(!$rootScope.loggedIn){
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: './partials/modules.html',
+      controller: "allModuleCtrl",
+      activetab: 'modules'
+    })
+    .when('/assignments', {
+      resolve: {
+        "check": function($location, $rootScope) {
+          if(!$rootScope.loggedIn){
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: './partials/assignments.html',
+      controller: "allAssignCtrl",
+      activetab: 'assignments'
+    })
+    .when('/modules/:moduleId', {
+      resolve: {
+        "check": function($location, $rootScope) {
+          if(!$rootScope.loggedIn){
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: './partials/module-detail.html',
+      controller: "moduleCtrl"
+    })
+    .when('/modules/:moduleId/:assignId', {
+      resolve: {
+        "check": function($location, $rootScope) {
+          if(!$rootScope.loggedIn){
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: './partials/assign-detail.html',
+      controller: "assignCtrl"
+    })
+    .when('/signup', {
+      templateUrl: './partials/signup.html',
+      controller: "NewUserController"
+    })
+    .when('/dashboard', {
+      resolve: {
+        "check": function($location, $rootScope) {
+          if(!$rootScope.loggedIn){
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: './partials/dashboard.html',
+      controller: "dashboardCtrl",
+      activetab: 'dashboard'
+    })
+    .when('/profile', {
+      resolve: {
+        "check": function($location, $rootScope) {
+          if(!$rootScope.loggedIn){
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: './partials/profile.html',
+      controller: "profileCtrl",
+      activetab: 'profile'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+  }]);
 
 app.controller('myCtrl', function($scope, $rootScope) {
-    $rootScope.loggedIn = false;
+  $rootScope.loggedIn = false;
 
-      $scope.logout = function(){
-        $rootScope.loggedIn = false;
-      };
+  $scope.logout = function(){
+    $rootScope.loggedIn = false;
+    $rootScope.username = null;
+  };
+});
+
+app.controller('profileCtrl', function($scope, $rootScope, $route) {
+  $scope.route = $route;
 });
 
 app.controller('loginCtrl', function($scope, $location, $rootScope, $http, UserService) {
-      
-      $scope.submit = function(){
-        var usrname = $scope.username;
-        var pass = $scope.password;
+  $scope.username = "admin";
+  $scope.password = "admin";
+  $scope.submit = function(){
+    var usrname = $scope.username;
+    var pass = $scope.password;
 
-          $scope.users = UserService.getUsers();
+    var users = UserService.getUsers();
 
-          for (x in $scope.users) {
-              if ($scope.users[x].username === $scope.username){
-                if($scope.users[x].password === $scope.password){
-                  var realUser = true;
-                }
-              }
-          }
-          if(realUser){
-          alert("Success!")
+    for (x in users) {
+      if (users[x].username === $scope.username){
+        if(users[x].password === $scope.password){
+          var realUser = true;
         }
+      }
+    }
+    if(realUser){
+      alert("Success!")
+    }
 
-        if(realUser) {
-            $rootScope.username = $scope.username
-            $rootScope.loggedIn = true;
-            $location.path('/dashboard');
-          } else {
-            alert("Incorrect username or password.");
-          }
-        
-      };
+    if(realUser) {
+      $rootScope.username = $scope.username
 
+      $rootScope.loggedIn = true;
+      $location.path('/dashboard');
+
+
+    } else {
+      alert("Incorrect username or password.");
+    }
+
+  };
+
+});
+
+app.controller('dashboardCtrl',
+  function($scope, $rootScope, $location, UserService, $route) {
+    $scope.$route = $route;
+    var username = $rootScope.username;
+
+    $scope.users = UserService.getUsers()
+    for (x in $scope.users){
+      if ($scope.users[x].username === $rootScope.username){
+        $scope.loggedInUser = $scope.users[x];
+      }
+    }
+
+    $scope.modules = $scope.loggedInUser.modules;
+
+  })
+
+app.controller('allModuleCtrl',
+  function ($scope, $rootScope, $location, UserService, $route, NewModuleService) {
+    $scope.module = {};
+    $scope.$route = $route;
+    var username = $rootScope.username;
+
+    var users = UserService.getUsers()
+    for (x in users){
+      if (users[x].username === $rootScope.username){
+        $scope.loggedInUser = users[x];
+      }
+    }
+    $scope.modules = $scope.loggedInUser.modules;
+    $scope.newModule = function (){
+      NewModuleService.newModule($scope.module);
+      $scope.module = new Module({});
+    }
   });
 
-// app.controller('signUpCtrl', function($scope, $location, $rootScope, $http, UserService, NewUserService) {
-//     $scope.signup = function(){
-//       var firstName = $scope.firstName;
-//       var secondName = $scope.secondName;
-//       var age = $scope.age;
-//       var email = $scope.email;
-//       var usrname = $scope.username;
-//       var pass = $scope.password;
-//       var pass2 = $scope.password2;
+app.controller('allAssignCtrl',
+  function ($scope, $rootScope, UserService, $route) {
+    $scope.$route = $route;
+    var modules = [];
+    $scope.modules = [];
+    modules = UserService.getModules();
+    var assignments = [];
+    for (x in modules){
+      for (y in modules[x].assignments){
+        assignments.push(modules[x].assignments[y])
+      }
+    }
+    $scope.assignments = assignments;
+    $scope.modules[0] = {};
+    $scope.modules.push.apply($scope.modules, modules);
+  }
+  );
 
-//       UserService.getUsers().success(function(data) {
-//         $scope.users = data;
-//         var logMeIn = true;
-//         for (x in $scope.users) {
-//               if ($scope.users[x].username === $scope.username){
-//                 alert("Sorry, username already taken.")
-//                 logMeIn = false;
-//                 break;
-//               }
-//               else {
-//                 if(pass != pass2){
-//                   alert("Sorry, passwords do not match.")
-//                   logMeIn = false;
-//                   break;
-//                 }
-//               }
-//           }
-//           if (logMeIn){
-//             var user = new User(userData);
-//             NewUserService.registerUser($scope.user); 
-//             $scope.users.push(user);
-//             $rootScope.username = $scope.username;
-//             $rootScope.loggedIn = true;
-//             $location.path('/dashboard');
-//             $rootScope.users = $scope.users;
-//           }
-//       })
-//     }
-//   });
-
-app.controller('dashboardCtrl', 
-  ['$scope', '$rootScope', '$location', 'UserService',
-    function($scope, $rootScope, $location, UserService) {
-      var username = $rootScope.username;
-
-      $scope.users = UserService.getUsers()
-        for (x in $scope.users){
-          if ($scope.users[x].username === $rootScope.username){
-            $scope.loggedInUser = $scope.users[x];
-          }
+app.controller('moduleCtrl', 
+  function ($scope, $rootScope, $routeParams, UserService, NewAssignmentService) {
+   var users = UserService.getUsers();
+   $scope.users = UserService.getUsers();
+   for (x in $scope.users){
+     if ($scope.users[x].username === $rootScope.username){
+      $scope.modules = $scope.users[x].modules;
+      for (y in $scope.modules){
+        if ($scope.modules[y].id === $routeParams.moduleId){
+          $scope.module = $scope.modules[y];
         }
-        
-        $scope.modules = $scope.loggedInUser.modules;
-      
-  }])
+      }
+    }
+  }
+  $rootScope.module = $scope.module;
+  $scope.assignments = $scope.module.assignments;
+  $scope.newAssignment = function (){
+    NewAssignmentService.newAssignment($scope.assignment);
+    $scope.assignment = new Assignment({});
+  }
+});
 
-app.service('NewUserService', function(UserService){
- var users = UserService.getUsers() || [];
+app.service('NewAssignmentService', function (AssignmentService, $rootScope, UserService, $routeParams){
+  $rootScope.users = UserService.getUsers();
+  $rootScope.modules = UserService.getModules();
+  this.newAssignment = function(assignmentData) {
+    var assignments = AssignmentService.getAssignments() || [];
+    assignments.push(new Assignment(assignmentData, assignments.length, $routeParams.moduleId));
+  }
+});
+
+app.service('NewModuleService', function (UserService){
+  this.newModule = function(moduleData) {
+    var modules = UserService.getModules() || [];
+    modules.push(new Module(moduleData, modules.length));
+  }
+});
+
+app.service('NewUserService', function (UserService){
   this.registerUser = function(userData) {
+    var users = UserService.getUsers() || [];
     users.push(new User(userData));
- // return users;
   }
 });
 
-app.controller('NewUserController', function($scope, NewUserService, $location, $rootScope){
+app.controller('NewUserController', function ($scope, NewUserService, $location, $rootScope, UserService){
+
   $scope.registerUser = function() {
-    NewUserService.registerUser($scope.user);
-    $rootScope.username = $scope.user.username;
-    $scope.user = new User({});
-    $rootScope.loggedIn = true;
-    $location.path('/dashboard');
-  }
-});
+    var users = UserService.getUsers();
+    var signup = true;
+    for (x in users){
+      if (users[x].username === $scope.user.username){
+        alert("Sorry, that username is already taken.")
+        signup = false;
+        break;
+      }
+      else if ($scope.user.password != $scope.user.password2){
+        alert("Sorry, your passwords must match.")
+        signup = false;
+        break;
+      }
+      if (signup) {
+        NewUserService.registerUser($scope.user);
+        $rootScope.username = $scope.user.username;
+        $scope.user = new User({});
+        $rootScope.loggedIn = true;
+        $location.path('/dashboard');
+      }
+    }
 
-app.controller('moduleCtrl', function ($scope, $rootScope, $routeParams, UserService) {
-             var users = UserService.getUsers();
-             $scope.users = UserService.getUsers();
-              for (x in $scope.users){
-                 if ($scope.users[x].username === $rootScope.username){
-                    $scope.modules = $scope.users[x].modules;
-                    for (y in $scope.modules){
-                      if ($scope.modules[y].id === $routeParams.moduleId){
-                        $scope.module = $scope.modules[y]
-                        ;
-                      }
-                    }
-          }
-        }
+    
+  }
 });
 
 app.controller('assignCtrl', function ($scope, $rootScope, $routeParams, UserService) {
-             var users = UserService.getUsers();
-             $scope.users = UserService.getUsers();
-              for (x in $scope.users){
-                 if ($scope.users[x].username === $rootScope.username){
-                    $scope.modules = $scope.users[x].modules;
-                    for (y in $scope.modules){
-                      if ($scope.modules[y].id === $routeParams.moduleId){
-                        $scope.assignments = $scope.modules[y].assignments;
-                        $scope.module = $scope.modules[y];
-                        $scope.assignment = $scope.assignments[$routeParams.assignId-1];
-                      }
-                    }
-          }
-        }
+ var users = UserService.getUsers();
+ $scope.users = UserService.getUsers();
+ for (x in $scope.users){
+   if ($scope.users[x].username === $rootScope.username){
+    $scope.modules = $scope.users[x].modules;
+    for (y in $scope.modules){
+      if ($scope.modules[y].id === $routeParams.moduleId){
+        $scope.assignments = $scope.modules[y].assignments;
+        $scope.module = $scope.modules[y];
+        $scope.assignment = $scope.assignments[$routeParams.assignId-1];
+      }
+    }
+  }
+}
 });
 
-app.factory('UserService', ['$http' , function($http){
-            var users = [];
-            $http.get('users.json').success(function(userData){
-                      userData.forEach(function(data){
-                        users.push(new User(data));
-                      });
-                    })
-            var api = {
-                getUsers : function() {
-                         return users;
-                }
-            }
-            return api
-        }]);
+app.factory('AssignmentService', ['$http', '$rootScope', '$routeParams', function ($http, $rootScope, $routeParams){
+
+  $rootScope.assignments = [];
+  var api = {
+    getAssignments : function() {
+      for (y in $rootScope.modules){
+        if ($rootScope.modules[y].id === $routeParams.moduleId){
+          var module = $rootScope.modules[y];
+          // $rootScope.module = module;
+        }
+      }
+      $rootScope.assignments = module.assignments;
+      return $rootScope.assignments;
+    }
+  }
+  return api;
+}]);
+
+app.factory('UserService', ['$http', '$rootScope', function ($http, $rootScope){
+  $rootScope.users = [];
+  $rootScope.modules = [];
+  $http.get('users.json').success(function(userData){
+    userData.forEach(function(data){
+      $rootScope.users.push(new User(data));
+    });
+  })
+  var api = {
+    getUsers : function() {
+     return $rootScope.users;
+   },
+   getModules : function() {
+    for (x in $rootScope.users){
+      if ($rootScope.users[x].username === $rootScope.username){
+        var user = $rootScope.users[x];
+      }
+    }
+    $rootScope.modules = user.modules;
+    return $rootScope.modules
+  }
+}
+return api
+}]);
 
 // user model
 function User(data) {
@@ -221,5 +332,22 @@ function User(data) {
   this.username = data.username || "",
   this.password = data.password || "",
   this.age = data.age || "",
+  this.course = data.course || "",
   this.modules = data.modules || []
+}
+
+function Module(data, modulesLength) {
+  this.id = (modulesLength+1).toString() || "",
+  this.title = data.title || "",
+  this.lecturer = data.lecturer || "",
+  this.assignments = data.assignments || []
+}
+
+function Assignment(data, assignmentsLength, moduleId) {
+  this.title = data.title || "",
+  this.snippet = data.snippet || "",
+  this.date = data.date || "",
+  this.moduleId = moduleId || "",
+  this.id = (assignmentsLength+1).toString() || "",
+  this.details = data.details || ""
 }
